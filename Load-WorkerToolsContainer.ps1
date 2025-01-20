a14320563c4badecb83f97aaab7ce6163cf0ef83
felsokning <46375514+felsokning@users.noreply.github.com>
2025-01-20 07:06:12 +0000

    Adding Docker Container with Tools from DockerHub to Script

--- Load-WorkerToolsContainer.ps1
+++ Load-WorkerToolsContainer.ps1
@@ -1,2 +1,2 @@
 # Prefer using PowerShell references.
-docker run --rm -it --workdir=/terraform --entrypoint="/bin/bash" -v ${PWD}:/terraform -v /var/run/docker.sock:/var/run/docker.sock docker.io/library/worker-tools:0.0.1
+docker run --rm -it --workdir=/terraform --entrypoint="/bin/bash" -v ${PWD}:/terraform -v /var/run/docker.sock:/var/run/docker.sock felsokning/worker-tools:4.0.0
